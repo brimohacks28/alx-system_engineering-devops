76d40a0534ba6e54676930db00edb84927fe3631
brian kyalo <brianmaema28@gmail.com>
2024-06-09 18:00:21 +0300

    Readme and the FGile

---
+++ 0x16-api_advanced/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# This is a sample Puppet manifest for the fix-wordpress module
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
